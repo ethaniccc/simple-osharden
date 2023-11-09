52210ff90f5ec8563eabc698f1f992c13ee069ec
ethaniccc <schooldocumentsandstuff@gmail.com>
2023-11-09 14:30:32 +0000

    utils/config.go: use temporary files while changing options

--- utils/config.go
+++ utils/config.go
@@ -2,13 +2,47 @@ package utils
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
+// CreateTempFileFrom will create a temp file from the given file.
+func CreateTempFileFrom(f string) (*os.File, error) {
+	file, err := os.Open(f)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open %s: %s", f, err.Error())
+	}
+	defer file.Close()
+
+	tmpFile, err := os.CreateTemp("", "osharden-temp")
+	if err != nil {
+		return nil, fmt.Errorf("unable to create temp file: %s", err.Error())
+	}
+
+	buffer, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read %s: %s", f, err.Error())
+	}
+
+	if _, err := tmpFile.Write(buffer); err != nil {
+		return nil, fmt.Errorf("unable to write to temp file: %s", err.Error())
+	}
+
+	return tmpFile, nil
+}
+
 // WriteOptsToFile will write the options specified to the given file.
 func WriteOptsToFile(opts map[string]string, sep string, file string) error {
-	buffer, err := os.ReadFile(file)
+	// Create a temp file, so in the case that the program crashes, we don't lose the original file.
+	f, err := CreateTempFileFrom(file)
+	if err != nil {
+		return fmt.Errorf("unable to create temp file: %s", err.Error())
+	}
+	defer f.Close()
+	defer os.Remove(f.Name())
+
+	buffer, err := os.ReadFile(f.Name())
 	if err != nil {
 		return fmt.Errorf("unable to read %s: %s", file, err.Error())
 	}
@@ -36,6 +70,7 @@ func WriteOptsToFile(opts map[string]string, sep string, file string) error {
 		lines = append(lines, fmt.Sprintf("%s%s%s", opt, sep, val))
 	}
 
+	// Write to the file.
 	if err := os.WriteFile(file, []byte(strings.Join(lines, "\n")), 0644); err != nil {
 		return fmt.Errorf("unable to write to %s: %s", file, err.Error())
 	}
